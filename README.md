4b8b87eac0ea0c53d086e0a52026eff276c93749
darealfoxwire121 <foxwire121@hotmail.com>
2021-02-27 17:10:50 +0000

    changed opencv to simplecv

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # **CV-Roblox**
 
-*OpenCV solutions for Roblox*
+*SimpleCV solutions for Roblox*
 
 ## **How does it work?**
 
-This project uses **OpenCV** to recognize roblox characters and other objects, and use the information gathered to produce a desired output.
+This project uses **SimpleCV** to recognize roblox characters and other objects, and use the information gathered to produce a desired output.
 It (will) consist of of multiple utilities to provide the roblox needs, including but not limited to:
 
 - [ ] Roblox Auto-Aim  
--- main.py
+++ main.py
@@ -1,26 +1,13 @@
-import cv2
-import keyboard as key
-import pyautogui
-import time
-import win32api
-
-def mouse_move(table):
-    pyautogui.moveTo(table[0],table[1])
-    pyautogui.click(table[0],table[1])
-
-def locate_img(image_files):
-    count = 0
-    for i in image_files:
-        count += 1
-        img_loc = pyautogui.locateCenterOnScreen(image_files[count], confidence=0.5)
-        if img_loc:
-            print('Found Object\n\n\n')
-            return img_loc
-
-win32api.MessageBox(None,"Make sure roblox is visible on screen, then press Ok", "Please note", 0)
-temp_imgs = ['C:/Users/foxwi/Documents/Lightshot/Screenshot_13.png','C:/Users/foxwi/Documents/Lightshot/Screenshot_12.png']
-
+from SimpleCV import Camera
+# Initialize the camera
+cam = Camera()
+# Loop to continuously get images
 while True:
-    time.sleep(.1)
-    Real_img = locate_img(temp_imgs)
-    mouse_move(Real_img)
+    # Get Image from camera
+    img = cam.getImage()
+    # Make image black and white
+    img = img.binarize()
+    # Draw the text "Hello World" on image
+    img.drawText("Hello World!")
+    # Show the image
+    img.show()
