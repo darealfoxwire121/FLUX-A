0cb9f0da726d74d337504915064ff058717e0d7b
darealfoxwire121 <foxwire121@hotmail.com>
2021-03-04 18:52:59 +0000

    Made it capture the roblox window, still checking for bugs

--- main.py
+++ main.py
@@ -1,12 +1,44 @@
 import cv2
 from PIL import ImageGrab
+import win32, win32api, win32gui, win32con, win32ui
 import numpy as np
 
+class CapWindow:
+    def _init_(Name_Of_Window):
+        w, h = 1920, 1080
 
-def FindTemplates(template_img, threshold):
+        Window_Name = Name_Of_Window or "Roblox"
+        hwnd = win32gui.FindWindow(None, Window_Name)
+
+        wDC = win32gui.GetWindowDC(hwnd)
+        dcObj = win32ui.CreateDCFromHandle(wDC)
+        cDC = dcObj.CreateCompatibleDC()
+        dataBitMap = win32ui.CreateBitmap()
+        dataBitMap.CreateCompatibleBitmap(dcObj, w, h)
+        cDC.SelectObject(dataBitMap)
+        cDC.BitBlt((0, 0), (w, h), dcObj, (0, 0), win32con.SRCCOPY)
+
+        signedIntsArray = dataBitMap.GetBitmapBits(True)
+        img = np.fromstring(signedIntsArray, dtype='uint8')
+        img.shape = (h, w, 4)
+
+        dcObj.DeleteDC()
+        cDC.DeleteDC()
+        win32gui.ReleaseDC(hwnd, wDC)
+        win32gui.DeleteObject(dataBitMap.GetHandle())
+
+        img = img[...,:3]
+
+        img = np.ascontiguousarray(img)
+
+        return img
+
+
+
+def FindTemplates(img, template_img, threshold):
 
     temp = cv2.imread(template_img)
-    img = np.array(ImageGrab.grab())
+    img = img
     result = cv2.matchTemplate(img, temp, cv2.TM_CCOEFF_NORMED)
 
     locations = np.where(result >= threshold)
@@ -32,4 +64,11 @@ def FindTemplates(template_img, threshold):
         print("Could not find template")
 
 
-FindTemplates("TEMPLATE MATCH IMAGE DIRECTORY HERE", 0.4)
+while True:
+    frame = CapWindow("Roblox")
+    temp_img = ""
+    FindTemplates(frame,temp_img, 0.4)
+
+    if cv2.waitKey() & 0xff ==ord('q'):
+        cv2.destroyAllWindows()
+        break
